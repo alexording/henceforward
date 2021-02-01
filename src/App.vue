5392f048934fe259560181cc8299878b4bc2766f
Thalida Noel <thalida.c.noel@gmail.com>
2021-02-01 18:15:06 -0500

    Updated Coming Soon page

--- src/App.vue
+++ src/App.vue
@@ -23,55 +23,56 @@ body {
 body {
   background-color: #fff;
   color: #000;
-  font: 400 0.625em/1.2 'Roboto', sans-serif;
+  font: 400 0.625em/1.2 'Merriweather', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
 
 h1,
-h2 {
+h2,
+p {
  margin: 0;
  padding: 0;
 }
 
-h1.wordmark {
-  font: 400 8.0em 'Jomhuria', sans-serif;
-  text-transform: uppercase;
-  padding-top: 0.2em;
-  line-height: 50%;
-}
+// h1.wordmark {
+//   font: 400 8.0em 'Jomhuria', sans-serif;
+//   text-transform: uppercase;
+//   padding-top: 0.2em;
+//   line-height: 50%;
+// }
 
-p.p1 {
-  font: 400 8.0em/0.8 'Jomhuria', sans-serif;
-  text-transform: lowercase;
-  margin: 0;
-}
+// p.p1 {
+//   font: 400 8.0em/0.8 'Jomhuria', sans-serif;
+//   text-transform: lowercase;
+//   margin: 0;
+// }
 
-h2 {
-  font-size: 2.0em;
-  font-weight: 300;
-  padding-bottom: 1em;
-}
+// h2 {
+//   font-size: 2.0em;
+//   font-weight: 300;
+//   padding-bottom: 1em;
+// }
 
-.highlight {
-  color: #ff0000;
-}
+// .highlight {
+//   color: #ff0000;
+// }
 
-@media screen and (max-width: 600px) {
-  body {
-    font-size: 0.3125em;
-  }
+// @media screen and (max-width: 600px) {
+//   body {
+//     font-size: 0.3125em;
+//   }
 
-  h1.wordmark {
-    font-size: 9.0em;
-  }
+//   h1.wordmark {
+//     font-size: 9.0em;
+//   }
 
-  p.p1 {
-    font-size: 9.0em;
-  }
+//   p.p1 {
+//     font-size: 9.0em;
+//   }
 
-  h2 {
-    font-size: 3.0em;
-  }
-}
+//   h2 {
+//     font-size: 3.0em;
+//   }
+// }
 </style>
---
+++ src/views/ComingSoon.vue
@@ -0,0 +1,237 @@
+<template>
+  <div class="coming-soon">
+    <section class="coming-soon__intro">
+      <div class="arrows"></div>
+      <h1 class="wordmark">Henceforward,</h1>
+      <p class="about">
+        we give more. We give smarter – to give social equity and racial justice
+        a chance to be realized in our lifetime.
+      </p>
+    </section>
+
+    <!-- Begin Mailchimp Signup Form -->
+    <div id="mc_embed_signup" class="coming-soon__form--wrapper">
+      <h2 class="coming-soon__form__header">Get notified when we launch.</h2>
+      <form
+        action="https://henceforward.us17.list-manage.com/subscribe/post?u=5e29881f7dd3fb2b1c3468c95&amp;id=4006e5e4cc"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        class="coming-soon__form validate"
+        target="_blank"
+        novalidate>
+        <div class="coming-soon__form__field mc-field-group">
+          <input
+            type="text"
+            placeholder="Name"
+            value=""
+            name="NAME"
+            class="coming-soon__form__input required"
+            id="mce-NAME">
+        </div>
+        <div class="coming-soon__form__field mc-field-group">
+          <input
+            type="email"
+            placeholder="Email"
+            value=""
+            name="EMAIL"
+            class="coming-soon__form__input required email"
+            id="mce-EMAIL">
+        </div>
+        <div id="mce-responses" class="clear">
+          <div class="response" id="mce-error-response" style="display:none"></div>
+          <div class="response" id="mce-success-response" style="display:none"></div>
+        </div>
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_5e29881f7dd3fb2b1c3468c95_4006e5e4cc" tabindex="-1" value=""></div>
+        <input type="submit" value="Count me in" name="subscribe" id="mc-embedded-subscribe" class="coming-soon__form__button">
+      </form>
+    </div>
+    <!--End mc_embed_signup-->
+
+    <footer class="coming-soon__footer">
+      insta logo
+      twitter logo
+      EST 2020
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComingSoon',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.coming-soon {
+  display: flex;
+  flex-flow: column nowrap;
+  min-width: 100%;
+  min-height: 100%;
+
+  &__intro,
+  &__form--wrapper {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+
+  &__intro {
+    display: flex;
+    flex-flow: column nowrap;
+    flex: 0 0 auto;
+
+    padding-top: 30px;
+    padding-bottom: 30px;
+
+    font: 400 4.5em 'Jomhuria', sans-serif;
+    line-height: 80%;
+
+    .arrows {
+      margin-bottom: 30px;
+      width: 44px;
+      height: 20px;
+      background-image: url(../assets/arrows_large.png);
+      background-repeat: no-repeat;
+      background-position: top left;
+      background-size: contain;
+    }
+
+    .wordmark {
+      font-weight: 400;
+      color: #130729;
+      text-transform: uppercase;
+    }
+
+    .about {
+      color: #A19CA9;
+    }
+  }
+
+  &__form {
+    display: flex;
+    flex-flow: row wrap;
+
+    &--wrapper {
+      display: flex;
+      flex-flow: column nowrap;
+      flex: 1 1 auto;
+      padding-top: 20px;
+      padding-bottom: 20px;
+      background: #130729;
+    }
+
+    &__header {
+      margin: 0 0 15px 0;
+      font: 400 1.4em 'Merriweather Sans', sans-serif;
+      color: #fff;
+    }
+
+    &__field {
+      flex: 0 1 auto;
+      padding: 0;
+      margin: 0 10px 0 0;
+      height: 100%;
+      width: 100%;
+      max-width: 210px;
+      min-width: 200px;
+    }
+
+    &__input {
+      height: 45px;
+      width: 100%;
+      padding: 12px;
+      margin: 0;
+      border: 1px solid #fff;
+      background-color: #fff;
+      font: 500 1.2em/1.2 'Merriweather', sans-serif;
+
+      &:focus {
+        outline: none;
+        border: 1px solid #ff0000;
+      }
+    }
+
+    &__button {
+      flex: 0 1 auto;
+      height: 45px;
+      width: auto;
+      margin: 0;
+      padding: 15px 20px;
+      border: 0;
+      border-radius: 20px;
+      background: #42807C;
+      font: 400 1.2em 'Merriweather Sans', sans-serif;
+      color: #fff;
+      text-transform: uppercase;
+      cursor: pointer;
+
+      &:focus {
+        outline: none;
+        border: 1px solid #ff0000;
+      }
+
+      &:hover {
+        opacity: 0.8;
+      }
+    }
+  }
+
+  &__footer {
+    display: flex;
+    flex-flow: row nowrap;
+    flex: 0 0 auto;
+    background: #130729;
+  }
+
+  @media screen and (max-width: 600px) {
+    &__form__field {
+      max-width: 100%;
+      margin-right: 0;
+      margin-bottom: 10px;
+    }
+  }
+
+  @media screen and (min-width: 375px) {
+    &__intro {
+      padding-top: 50px;
+      padding-bottom: 50px;
+
+      .arrows {
+        margin-bottom: 50px;
+      }
+
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    &__intro,
+    &__form--wrapper {
+      padding-left: 100px;
+      padding-right: 100px;
+    }
+
+    &__intro {
+      padding-top: 80px;
+      padding-bottom: 80px;
+      font-size: 9.0em;
+
+      .arrows {
+        margin-bottom: 80px;
+        width: 81px;
+        height: 37px;
+      }
+    }
+
+    &__form--wrapper {
+      padding-top: 50px;
+      padding-bottom: 50px;
+    }
+
+  }
+}
+</style>
